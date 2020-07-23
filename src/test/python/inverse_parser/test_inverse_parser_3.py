bfb375c55aa2a286057837c447b9d7517514bfc1
aniskoff <cme412@mail.ru>
2020-07-23 23:23:22 +0300

    simple test for inverse_parser_3

---
+++ src/test/python/inverse_parser/test_inverse_parser_3.py
@@ -0,0 +1,40 @@
+# Copyright (c) Aniskov N.
+
+import os
+
+import pytest
+
+from src.main.python.inverse_parser.inverse_parser_3 import *
+from src.main.util.const import TEST_RESOURCES_PATH
+
+
+class TestInverseParser3:
+
+    _input_dir = os.path.join(TEST_RESOURCES_PATH, 'inverse_parser/inverse_parser_3')
+
+    test_cases_dirs = \
+        [
+            os.path.join(_input_dir, 'case_for'),
+
+            os.path.join(_input_dir, 'case_import'),
+
+            os.path.join(_input_dir, 'case_simple_ops')
+
+        ]
+
+    # Tests:
+
+    @pytest.mark.parametrize(
+        'cases_dirs',
+        test_cases_dirs
+    )
+    def test_generated_code_is_eq_to_original(self, cases_dirs: str) -> None:
+        for entry in os.scandir(cases_dirs):
+            xml_ast_str = json2xml(parse_file(entry.path))
+            inverse_parser = InverseParser(xml_ast_str, from_string=True)
+
+            with open(entry.path) as in_f:
+                real_source = in_f.read()
+            generated_source = inverse_parser.get_source()
+
+            assert real_source == generated_source
