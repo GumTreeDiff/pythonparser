300b72e950e1b7e576f44a935847d8cf1cf5311e
aniskoff <cme412@mail.ru>
2020-08-03 22:02:13 +0300

    added simple test cases for new implemented features

---
+++ src/test/resources/inverse_parser/inverse_parser_3/case_class_def/simple_class_def.py
@@ -0,0 +1,3 @@
+@dec1
+class foo(base1, base2, metaclass=meta):
+    pass
--- src/test/resources/inverse_parser/inverse_parser_3/case_function_def/simple_function_def.py
+++ src/test/resources/inverse_parser/inverse_parser_3/case_function_def/simple_function_def.py
@@ -1,4 +1,3 @@
 @dec1
-@dec2
-def f(a, b=1, c=2, *d, e, f=3, **g) -> 'return annotation':
+def f(a, b=1, c=2, *d, e, f=3, **g):
     pass
