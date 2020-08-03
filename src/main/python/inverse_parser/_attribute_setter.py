4fe345c3469073d3ba7663d979d7478d4777fdb0
aniskoff <cme412@mail.ru>
2020-08-03 22:04:07 +0300

    refactored code a bit

--- src/main/python/inverse_parser/_attribute_setter.py
+++ src/main/python/inverse_parser/_attribute_setter.py
@@ -3,13 +3,15 @@ import ast
 import xml.etree.ElementTree as ET
 from pydoc import locate
 
+import src.main.python.inverse_parser._node_restorer as nr
+from src.main.python.inverse_parser._xml_node_children_getter import _XmlNodeChildrenGetter
 from src.main.util.const import DEFAULT_ENCODING
 
 
-class _ValueSetter:
+class _AttributeSetter:
 
     @staticmethod
-    def set_value(xml_node: ET.Element, py_node: ast.AST, py_node_attrib_name: str) -> None:
+    def set_const_value(xml_node: ET.Element, py_node: ast.AST, py_node_attrib_name: str) -> None:
         """
         :param xml_node:
         :param py_node:
@@ -36,6 +38,17 @@ class _ValueSetter:
             value = value_type(str_val_repr)
 
         setattr(py_node, py_node_attrib_name, value)
+
+    @staticmethod
+    def set_list_field(xml_node: ET.Element, py_node: ast.AST, py_node_field_name: str) -> None:
+        xml_node_of_field = _XmlNodeChildrenGetter.get_unique_child(xml_node, with_tag=py_node_field_name)
+        if xml_node_of_field is not None:
+            xml_node_of_field_children = _XmlNodeChildrenGetter.get_children(xml_node_of_field)
+            setattr(py_node, py_node_field_name, nr._NodeRestorer.restore_many(xml_node_of_field_children))
+        else:
+            setattr(py_node, py_node_field_name, [])
+
+
 
 
 
