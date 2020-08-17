4060fd8818c78d53525c1e2b78648367128e9308
aniskoff <cme412@mail.ru>
2020-08-17 21:17:51 +0300

    added logger

--- src/main/python/inverse_parser/_xml_node_children_getter.py
+++ src/main/python/inverse_parser/_xml_node_children_getter.py
@@ -1,5 +1,12 @@
 # Copyright (c) Aniskov N.
 
+import logging
+
+from src.main.util.const import LOGGER_NAME
+from src.main.util.log_util import log_and_raise_error
+
+logger = logging.getLogger(LOGGER_NAME)
+
 
 class _XmlNodeChildrenGetter:
     @staticmethod
@@ -15,7 +22,9 @@ class _XmlNodeChildrenGetter:
         if not res:
             return None
         if len(res) > 1:
-            raise RuntimeError(f'{len(res)} children of {xml_node.tag} with tag={with_tag} found.'
-                               f' But {with_tag} expected to be unique')
+            log_and_raise_error(f'{len(res)} children of {xml_node.tag} with tag={with_tag} found.'
+                                f' But {with_tag} expected to be unique',
+                                logger,
+                                RuntimeError)
         else:
             return res[0]
