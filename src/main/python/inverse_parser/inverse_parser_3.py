4caf02715034ec93be1c2ceaa5832753d58d1fa9
Anastasiia.Birillo <nbirillo@mail.ru>
2020-09-14 10:06:57 +0300

    Change inverse parser: add the possibility to run it from the command line

--- src/main/python/inverse_parser/inverse_parser_3.py
+++ src/main/python/inverse_parser/inverse_parser_3.py
@@ -3,7 +3,8 @@
 # Copyright (c) Aniskov N.
 
 import sys
-sys.path.append('../../../..')
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))))
 
 import argparse
 import ast
